68b87b2b3cb84ccf27a76b932745917623d5e30f
ssarari <ogg05050@gmail.com>
2024-09-06 17:16:32 +0900

    2024-09-06

---
+++ test2/src/main/java/com/example/test2/controller/SurveyFoodController.java
@@ -0,0 +1,34 @@
+package com.example.test2.controller;
+
+import com.example.test2.entity.SurveyFood;
+import com.example.test2.service.SurveyFoodService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/survey")
+public class SurveyFoodController {
+    @Autowired
+    private SurveyFoodService service;
+
+    @GetMapping("/foods")
+    public String getRandomFoods(Model model){
+        List<SurveyFood> randomFoodsKorea = service.getRadomFoodsByType(SurveyFood.FoodType.valueOf("한식"), 5);
+        List<SurveyFood> randomFoodsChinese = service.getRadomFoodsByType(SurveyFood.FoodType.valueOf("중식"), 5);
+        List<SurveyFood> randomFoodsJapan = service.getRadomFoodsByType(SurveyFood.FoodType.valueOf("일식"), 5);
+        List<SurveyFood> randomFoodsWestern = service.getRadomFoodsByType(SurveyFood.FoodType.valueOf("양식"), 5);
+        model.addAttribute("koreaFoods", randomFoodsKorea);
+        model.addAttribute("chineseFoods", randomFoodsChinese);
+        model.addAttribute("japanFoods", randomFoodsJapan);
+        model.addAttribute("westernFoods", randomFoodsWestern);
+        return "intensify/survey";
+    }
+
+}
---
+++ test2/src/main/resources/static/assets/js/navermap.js
@@ -0,0 +1,8 @@
+//네이버 지도 스크립트
+
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+};
+
+var map = new naver.maps.Map('map', mapOptions);
