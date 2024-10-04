58d139ce56f07cbc44080542829ae136b15eb810
guntae <simgun99@gmail.com>
2024-10-04 18:36:36 +0900

    1004_음식점 정보 1. 음식점 정보 DB 업데이트 2. 음식점 마커 클릭시 음식점 정보 조회 3. 웹 크기별 음식점 크기 CSS 지정 4. MarkerClustering.js 추가 5. Restaurant.java 엔티티 컬럼 매핑

--- test2/src/main/java/com/example/test2/entity/Restaurant.java
+++ test2/src/main/java/com/example/test2/entity/Restaurant.java
@@ -31,4 +31,31 @@ public class Restaurant {
 
     @Column(name ="y")
     private double y;
+
+    @Column(name = "total_reviews")
+    private long total_reviews;
+
+    @Column(name = "positive_reviews")
+    private long positive_reviews;
+
+    @Column(name = "negative_reviews")
+    private long negative_reviews;
+
+    @Column(name = "revisit_above_3")
+    private long revisit_above_3;
+
+    @Column(name = "final_score")
+    private double final_score;
+
+    @Column(name = "tag1")
+    private String tag1;
+
+    @Column(name = "tag2")
+    private String tag2;
+
+    @Column(name = "tag3")
+    private String tag3;
+
+    @Column(name = "category")
+    private String category;
 }
